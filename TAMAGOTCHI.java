d63c3c10d62eb4ae13d16eb19503edd4f1e03639
GerardoCL31 <gerardotrajano@gmail.com>
2024-11-29 11:28:06 +0100

    ej2

---
+++ TAMAGOTCHI.java
@@ -0,0 +1,12 @@
+public class TAMAGOTCHI {
+
+    private int estado;
+
+    public static void main(System[]args){
+        if (estado=8) {
+            System.System.out.println(:-);
+            
+        }
+
+    }
+}
