4e4276bcf3c88e05a6ebd985d8016d667c7f8858
unknown <gerardotrajano@gmail.com>
2024-11-29 22:17:19 +0100

    se añade tamagochi

--- TAMAGOTCHI.java
+++ TAMAGOTCHI.java
@@ -1,12 +1,58 @@
-public class TAMAGOTCHI {
+public class taamagochi {
+	
 
-    private int estado;
+    private int hunger;
+    private int energy;
+    private int mood;
+    
+    
+    
+    
+    public taamagochi() {
+        this.hunger=4;
+        this.energy=4;
+        this.mood=4;
+    }
 
-    public static void main(System[]args){
-        if (estado=8) {
-            System.System.out.println(:-);
-            
-        }
-
+    public String play(){
+        hunger++;
+        energy--;
+        mood++;
+        
+        return getState();
+        
+        
+    }
+    
+    public String eat() {
+        hunger--;
+        hunger--;
+        energy--;
+        return getState();
+        
+    }
+    
+    public String sleep() {
+        energy++;
+        energy++;
+        
+        return getState();
+    }
+    
+    
+public String getState() {
+    if (energy == 0) {
+        return "(-_-) zZZ";
+    }else if (mood>8) {
+        return ":-)";
+    }else if(mood<2) {
+        return "ఠ_ఠ";
+    }else if (energy<2) {
+        return "(-_-)";
+    }else {
+        return ":-|";
+    }
+        
+        
     }
 }
---
+++ tamaMain.java
@@ -0,0 +1,11 @@
+public class taamaMain{
+    public static void main(String[]args){
+        taamagochi tama1 = new taamagochi();
+		
+		System.out.println(tama1.play());
+		System.out.println(tama1.eat());
+		System.out.println(tama1.sleep());
+	}
+
+}
+
